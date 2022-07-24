13d235b31ff0466bb918e94cf17681f159c5941b
Olexandr Tolochko <0504fusion@gmail.com>
2022-07-24 20:51:09 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,7 +3,7 @@ import throttle from "lodash.throttle";
 // ==============================
 
 const form = document.querySelector('.feedback-form');
-console.log(form);
+// console.log(form);
 
 // ==============================
 
